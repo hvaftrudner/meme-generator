509c6f2db315766abbfa2d77a89971519c243c48
hvaftrudner <krill_eriksson@hotmail.com>
2020-11-25 23:01:05 +0100

    Initial Commit

---
+++ meme generator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  meme generator
+//
+//  Created by Kristoffer Eriksson on 2020-11-25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
